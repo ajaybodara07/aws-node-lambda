fc87c6127f41998e62fcda303343fff021253d83
ajay bodara <ad.bodara@gmail.com>
2021-06-06 20:21:14 +0530

    AWS-Lambda

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# aws-node-lambda
+# AWS Lambda Function Example for Node.js
+
+- https://docs.aws.amazon.com/lambda/latest/dg/welcome.html
+- https://docs.aws.amazon.com/lambda/latest/dg/lambda-nodejs.html
+
+# use Direct AWS Lambda Function
+
+- https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html
---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+ * This file is an example AWS Lambda function.
+ */
+const moesif = require('moesif-aws-lambda');
+const https = require('https');
+console.log('Loading function');
+
+const moesifOptions = {
+
+    applicationId: process.env.MOESIF_APPLICATION_ID,
+
+    identifyUser: function (event, context) {
+        return event.requestContext && event.requestContext.identity && event.requestContext.identity.cognitoIdentityId
+    }
+};
+
+var moesifMiddleware = moesif(moesifOptions);
+moesifMiddleware.startCaptureOutgoing();
+
+exports.handler = function (event, context, callback) {
+    // Outgoing API call to third party
+    https.get(
+        {
+          host: 'jsonplaceholder.typicode.com',
+          path: '/posts/1'
+        },
+        function(res) {
+          var body = '';
+          res.on('data', function(d) {
+            body += d;
+          });
+
+          res.on('end', function() {
+            var parsed = JSON.parse(body);
+            console.log(parsed);
+          });
+        }
+      );
+
+    callback(null, {
+        statusCode: '200',
+        body: JSON.stringify({key: 'hello world'}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+// Async Functions 
+// For more details, please refer to - https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html.
+
+// exports.handler = async (event, context) => {
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: 'hello world' })
+//   }
+//   return response
+// }
+
+exports.handler = moesif(moesifOptions, exports.handler);
